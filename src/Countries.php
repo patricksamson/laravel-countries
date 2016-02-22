efa800d1fd781ddc87de61efd39adb72b1a074a2
Patrick Samson <patricksamson236@gmail.com>
2016-02-21 19:47:39 -0500

    throw exceptions

--- src/Countries.php
+++ src/Countries.php
@@ -38,18 +38,18 @@ class Countries
         } else if (file_exists(self::$DEV_PATH)){
             $this->data = json_decode(file_get_contents(self::$DEV_PATH), true);
         } else {
-            return; // we should throw an exception here...
+            throw new Exception(sprintf('Cannot find the file "%s".', self::$PROD_PATH));
         }
     }
 
     public function getByAlpha2Code($code)
     {
-        return $this->searchSingleItemByColumn(self::ISO3166_ALPHA_2, $code);
+        return $this->searchSingleItemByColumn(self::ISO3166_ALPHA_2, strtoupper($code));
     }
 
     public function getByAlpha3Code($code)
     {
-        return $this->searchSingleItemByColumn(self::ISO3166_ALPHA_3, $code);
+        return $this->searchSingleItemByColumn(self::ISO3166_ALPHA_3, strtoupper($code));
     }
 
     public function getByNumericCode($code)
@@ -74,7 +74,7 @@ class Countries
         return $this->data[$key];
     }
 
-    public function searchMultipleItemsByColumn($columnKey, $input)
+    protected function searchMultipleItemsByColumn($columnKey, $input)
     {
         $keys = array_flip(array_keys(array_column($this->data, $columnKey), $input));
 

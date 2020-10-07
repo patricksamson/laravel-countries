648d6af0a53d498156528fd1d63deca991a95de7
Patrick Samson <patricksamson236@gmail.com>
2020-10-07 17:30:06 -0400

    Add a config to choose the compile data or a json file

---
+++ src/config/laravel-countries.php
@@ -0,0 +1,15 @@
+<?php
+
+return [
+    /*
+    |--------------------------------------------------------------------------
+    | Countries Data Source
+    |--------------------------------------------------------------------------
+    |
+    | This option controls the source of the data used by this package.
+    |
+    */
+
+    'countries_json_path' => null,
+    //'countries_json_path' => base_path('vendor/mledoze/countries/dist/countries-unescaped.json');
+];
